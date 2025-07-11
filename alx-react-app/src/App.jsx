2beec8c7b5a3714eb3bb2250ddc23d0e6abf68d9
RowinaHemida <eng.rowinahemida@gmail.com>
2025-07-11 21:58:55 +0300

    Final version with all components and props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,17 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
-
-import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
-      <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
