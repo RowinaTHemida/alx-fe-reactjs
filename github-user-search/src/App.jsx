7bd1982ac2d36555bbf319c6e6f51bc85c316e1a
RowinaHemida <eng.rowinahemida@gmail.com>
2025-08-04 01:05:30 +0300

    Task 0: Initialize GitHub User Search React project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>GitHub User Search</h1>
+      <p>Start typing a GitHub username to search...</p>
+    </div>
+  );
+}
+
+export default App;
