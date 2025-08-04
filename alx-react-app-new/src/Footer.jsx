3ec0bee75522a500df58cd0839c520ebf5bca6ac
RowinaHemida <eng.rowinahemida@gmail.com>
2025-08-04 22:21:51 +0300

    Apply inline CSS styling to all components

---
+++ alx-react-app-new/src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#333',
+      color: 'white',
+      textAlign: 'center',
+      padding: '10px',
+      marginTop: '20px'
+    }}>
+      <p>© 2025 Rowina Tamer. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
