f248b97eb8f883491c458ca261637498d2abb280
RowinaHemida <eng.rowinahemida@gmail.com>
2025-08-04 18:53:56 +0300

    🧹 Cleaned old files

--- alx-react-app/README.md
+++
@@ -1,2 +0,0 @@
-# ALX React App
-This is my first React app created using Vite for the ALX Front-End project.
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>© 2025 Recipe Sharing App. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
