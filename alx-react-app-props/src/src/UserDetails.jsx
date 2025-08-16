f58d35229aff489654c392a3e0f60c3be3b8b8e4
Rowina Tamer Hemida <eng.rowinahemida@gmail.com>
2025-08-16 19:53:30 +0300

    feat(alx-react-app-props): refactor prop drilling to Context API

---
+++ alx-react-app-props/src/src/UserDetails.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  if (!userData) return null;
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
+
