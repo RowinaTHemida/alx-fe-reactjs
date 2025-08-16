a8ea4807af5be42a2346afa60e06d93d2fa0944b
Rowina Tamer Hemida <eng.rowinahemida@gmail.com>
2025-08-16 17:11:13 +0300

    fix: final inline-styled components with correct content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function MainContent() {
   return (
     <main style={{ padding: "20px", fontSize: "18px", color: "darkblue" }}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+      <p>"I love to visit New York, Paris, and Tokyo."</p>
     </main>
   );
 }
